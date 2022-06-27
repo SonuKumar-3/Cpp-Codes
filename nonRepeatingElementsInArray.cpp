28cf0851a76e4f319f580ba94e54839cb9320546
SONU KUMAR <95914986+SonuKumar-3@users.noreply.github.com>
2022-06-28 00:25:08 +0530

    Find all the non-repeating elements in an array

---
+++ nonRepeatingElementsInArray.cpp
@@ -0,0 +1,34 @@
+/*  Problem Statement: Find all the non-repeating elements for a given array. Outputs can be in any order.
+    Time Complexity: O(N)
+    Space Complexity: O(N)
+*/
+#include <iostream>
+#include <map>
+using namespace std;
+
+#define loop(i, a, b) for(int i = a; i < b; i++)
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+
+    map<int, int> mp;
+    map<int, int>::iterator it;
+
+    loop(i, 0, n)
+    {
+        cin>>arr[i];
+        mp[arr[i]]++;
+    }
+
+    for(it = mp.begin(); it != mp.end(); it++)
+    {
+        if(it->second == 1)
+            cout<<it->first<<" ";
+    }
+
+    cout<<endl;
+    return 0;
+}
